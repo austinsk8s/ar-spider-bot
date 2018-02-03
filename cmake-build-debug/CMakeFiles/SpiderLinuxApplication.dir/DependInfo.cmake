b1a85fb90cf71e50b86feab7203fe32233a2f515
Austin Williams <austinsk8s2day@gmail.com>
2018-02-02 20:06:45 -0600

    added initial files

---
+++ cmake-build-debug/CMakeFiles/SpiderLinuxApplication.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/ADC.cpp" "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/cmake-build-debug/CMakeFiles/SpiderLinuxApplication.dir/ADC.cpp.o"
+  "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/ADCTest.cpp" "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/cmake-build-debug/CMakeFiles/SpiderLinuxApplication.dir/ADCTest.cpp.o"
+  "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/BtSPP.cpp" "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/cmake-build-debug/CMakeFiles/SpiderLinuxApplication.dir/BtSPP.cpp.o"
+  "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/BtSppCommand.cpp" "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/cmake-build-debug/CMakeFiles/SpiderLinuxApplication.dir/BtSppCommand.cpp.o"
+  "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/CMotor.cpp" "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/cmake-build-debug/CMakeFiles/SpiderLinuxApplication.dir/CMotor.cpp.o"
+  "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/CSpider.cpp" "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/cmake-build-debug/CMakeFiles/SpiderLinuxApplication.dir/CSpider.cpp.o"
+  "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/CSpiderLeg.cpp" "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/cmake-build-debug/CMakeFiles/SpiderLinuxApplication.dir/CSpiderLeg.cpp.o"
+  "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/Main.cpp" "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/cmake-build-debug/CMakeFiles/SpiderLinuxApplication.dir/Main.cpp.o"
+  "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/ManualControl.cpp" "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/cmake-build-debug/CMakeFiles/SpiderLinuxApplication.dir/ManualControl.cpp.o"
+  "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/PIO_BUTTON.cpp" "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/cmake-build-debug/CMakeFiles/SpiderLinuxApplication.dir/PIO_BUTTON.cpp.o"
+  "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/PIO_LED.cpp" "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/cmake-build-debug/CMakeFiles/SpiderLinuxApplication.dir/PIO_LED.cpp.o"
+  "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/Project.cpp" "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/cmake-build-debug/CMakeFiles/SpiderLinuxApplication.dir/Project.cpp.o"
+  "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/Queue.cpp" "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/cmake-build-debug/CMakeFiles/SpiderLinuxApplication.dir/Queue.cpp.o"
+  "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/QueueCommand.cpp" "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/cmake-build-debug/CMakeFiles/SpiderLinuxApplication.dir/QueueCommand.cpp.o"
+  "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/mmap.cpp" "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/cmake-build-debug/CMakeFiles/SpiderLinuxApplication.dir/mmap.cpp.o"
+  "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/terasic_os.cpp" "/Users/austinsk8s/projects/spider/SpiderRobotStarterKit/SpiderLinuxApplication/cmake-build-debug/CMakeFiles/SpiderLinuxApplication.dir/terasic_os.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/SpiderLinuxApplication.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SpiderLinuxApplication.dir/ADC.cpp.o"
+  "CMakeFiles/SpiderLinuxApplication.dir/ADCTest.cpp.o"
+  "CMakeFiles/SpiderLinuxApplication.dir/BtSPP.cpp.o"
+  "CMakeFiles/SpiderLinuxApplication.dir/BtSppCommand.cpp.o"
+  "CMakeFiles/SpiderLinuxApplication.dir/CMotor.cpp.o"
+  "CMakeFiles/SpiderLinuxApplication.dir/CSpider.cpp.o"
+  "CMakeFiles/SpiderLinuxApplication.dir/CSpiderLeg.cpp.o"
+  "CMakeFiles/SpiderLinuxApplication.dir/Main.cpp.o"
+  "CMakeFiles/SpiderLinuxApplication.dir/ManualControl.cpp.o"
+  "CMakeFiles/SpiderLinuxApplication.dir/mmap.cpp.o"
+  "CMakeFiles/SpiderLinuxApplication.dir/PIO_BUTTON.cpp.o"
+  "CMakeFiles/SpiderLinuxApplication.dir/PIO_LED.cpp.o"
+  "CMakeFiles/SpiderLinuxApplication.dir/Project.cpp.o"
+  "CMakeFiles/SpiderLinuxApplication.dir/Queue.cpp.o"
+  "CMakeFiles/SpiderLinuxApplication.dir/QueueCommand.cpp.o"
+  "CMakeFiles/SpiderLinuxApplication.dir/terasic_os.cpp.o"
+  "SpiderLinuxApplication.pdb"
+  "SpiderLinuxApplication"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SpiderLinuxApplication.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
